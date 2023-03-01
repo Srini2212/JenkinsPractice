19d028b1c196b7dc540fda66a5556b9718e48e87
Srini2212 <112047456+Srini2212@users.noreply.github.com>
2023-03-01 16:16:07 +0530

    This is an initial commit
    Adding the initial brief about presentation

---
+++ GoodMorning.java
@@ -0,0 +1,12 @@
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Good Morning to all");
+                System.out.println("Today we have presentation about Jenkins");
+                System.out.println("Jenkins is integration and deployment tool");  
+                System.out.println("Develops good integration");
+	}
+
+}
